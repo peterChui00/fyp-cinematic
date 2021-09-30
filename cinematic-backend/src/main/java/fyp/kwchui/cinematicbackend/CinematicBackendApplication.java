aad9096774c83f8e2ee0a55d8431c582cf434c3a
peterChuiGit <70317308+peterChuiGit@users.noreply.github.com>
2021-09-30 21:16:03 +0800

    Initial commit

---
+++ cinematic-backend/src/main/java/fyp/kwchui/cinematicbackend/CinematicBackendApplication.java
@@ -0,0 +1,13 @@
+package fyp.kwchui.cinematicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinematicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinematicBackendApplication.class, args);
+	}
+
+}
---
+++ cinematic-backend/src/test/java/fyp/kwchui/cinematicbackend/CinematicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fyp.kwchui.cinematicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinematicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
